308ca04d41e8e716f8442cbaac329c321a1c04dc
Aram Zhamkochyan <aramzham@hotmail.com>
2022-09-13 17:50:35 +0200

    Update Program.cs

--- ScrollingText/Ticker/Program.cs
+++ ScrollingText/Ticker/Program.cs
@@ -10,9 +10,9 @@ namespace Ticker
             Console.Title = "Running Line";
             Console.ForegroundColor = ConsoleColor.Cyan;
             var rl = new RunningLineCreator();
-            rl.Advertisments.Add("Վաճառվում է բնակարան Ղափանի կենտրոնում, կոդ DP154, գինը՝ պայմանագրային");
-            rl.Advertisments.Add("Իջևանի կարատեի դպրոցն անց է կացնում մարզումներ 4 տարեկանից բարձր անձանց համար");
-            rl.Advertisments.Add("Այստեղ կարող է լինել Ձեր գովազդը");
+            rl.AddAdvertisment("Վաճառվում է բնակարան Ղափանի կենտրոնում, կոդ DP154, գինը՝ պայմանագրային");
+            rl.AddAdvertisment("Իջևանի կարատեի դպրոցն անց է կացնում մարզումներ 4 տարեկանից բարձր անձանց համար");
+            rl.AddAdvertisment("Այստեղ կարող է լինել Ձեր գովազդը");
 
             rl.RunTheLine();
 
